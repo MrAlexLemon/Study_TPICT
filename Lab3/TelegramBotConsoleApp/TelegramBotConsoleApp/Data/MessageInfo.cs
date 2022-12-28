d857a0415f5d780f574da0a44e05cf93eb6fbf37
Alex <khomenkosasha99@gmail.com>
2022-12-28 14:09:41 +0200

    Lab3

---
+++ Lab3/TelegramBotConsoleApp/TelegramBotConsoleApp/Data/MessageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotConsoleApp.Data
+{
+    public class MessageInfo
+    {
+        public int Id { get; set; }
+        public long UserId { get; set; }
+        public long ChatId { get; set; }
+        public long MessageId { get; set; }
+        public string? Message { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
---
+++ Lab3/TelegramBotConsoleApp/TelegramBotConsoleApp/Migrations/20221213233736_initialDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramBotConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessageInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    ChatId = table.Column<long>(type: "bigint", nullable: false),
+                    MessageId = table.Column<long>(type: "bigint", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", maxLength: 4100, nullable: true),
+                    MessageDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageInfos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageInfos");
+        }
+    }
+}
